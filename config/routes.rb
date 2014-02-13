08fe60399528af67828fb60c3d7c803f4e701d08
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-02-13 10:35:26 +0100

    Added empty DashboardController.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ EbuOndemand::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'dashboard#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
