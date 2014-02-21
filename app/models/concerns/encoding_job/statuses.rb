21d626246d0f85f5cb15a11f4b09131efa3cae88
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-02-21 11:46:56 +0100

    Basic state machine logic for EncodingJob

--- app/models/concerns/encoding_job/statuses.rb
+++ app/models/concerns/encoding_job/statuses.rb
@@ -24,19 +24,81 @@ module EncodingJob::Statuses
   
   private
   
+  # State machine definition, this describes all possible transitions
   def transition_from_pending
     puts "from pending"
+    if requires_transcoding?
+      enter_transcoding
+    elsif requires_post_processing?
+      enter_post_processing
+    else
+      enter_conformance_checking
+    end
   end
   
   def transition_from_transcoding
     puts "from transcoding"
+    if did_complete_transcoding?
+      enter_pending
+    elsif did_fail_transcoding?
+      enter_failed
+    end
   end
   
   def transition_from_post_processing
     puts "from post processing"
+    if did_complete_post_processing?
+      enter_pending
+    elsif did_fail_post_processing?
+      enter_failed
+    end
   end
   
   def transition_from_conformance_checking
     puts "from conformance checking"
+    if did_finish_conformance_checking?
+      enter_success
+    end
+  end
+  
+  # Status checking methods, used to determine preconditions for transitions
+  def requires_transcoding?
+  end
+
+  def requires_post_processing?
+  end
+
+  def did_complete_transcoding?
+  end
+  
+  def did_fail_transcoding?
+  end
+  
+  def did_complete_post_processing?
+  end
+  
+  def did_fail_post_processing?
+  end
+  
+  def did_finish_conformance_checking?
+  end
+  
+  # Entering a state triggers these actions
+  def enter_pending
+  end
+
+  def enter_transcoding
+  end
+  
+  def enter_post_processing
+  end
+  
+  def enter_conformance_checking
+  end
+  
+  def enter_failed
+  end
+  
+  def enter_success
   end
 end
