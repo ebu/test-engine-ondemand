3dd5adbfc47f84687c4def8b679de21b8bf039c7
Sjoerd Tieleman <sjoerd@madebyhiro.com>
2014-07-09 11:22:53 +0200

    Updated Dash.js to 1.1.2.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,6 +1,10 @@
 # Serves as a fallback for development to serve media files. Not used in production.
 class MediaController < ApplicationController
   def serve
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    response.headers['Access-Control-Allow-Methods'] = "GET, OPTIONS, HEAD"
+    response.headers['Access-Control-Allow-Headers'] = "Origin, Content-Type, Accept, Range"
+    
     file = [Rails.root, 'public', params[:other]].join(File::SEPARATOR)
     if File.exist?(file)
       send_file file
