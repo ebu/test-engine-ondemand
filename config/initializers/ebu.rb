322f068da981c59d6248856e66313471699384da
Sjoerd Tieleman <sjoerd@madebyhiro.com>
2015-01-13 14:52:42 +0100

    Removed development_remote env.

--- config/initializers/ebu.rb
+++ config/initializers/ebu.rb
@@ -34,7 +34,6 @@ module EBU
   # Callback URL for Codem transcoder. Depends on Rails environment and setup.
   CALLBACK_URL_FOR_CODEM = case Rails.env
     when 'development' then 'http://localhost:3000/codem_notifications'
-    when 'development_remote' then 'http://localhost:3000/codem_notifications'
     when 'production' then 'http://on-demand.ebu.io/codem_notifications'
     else raise "Codem callback URL not yet configured."
   end
@@ -42,7 +41,6 @@ module EBU
   # Callback URL for http-runner jobs. Depends on Rails environment and setup.
   CALLBACK_URL_FOR_HTTP_RUNNER = case Rails.env
     when 'development' then 'http://localhost:3000/http_runner_notifications'
-    when 'development_remote' then 'http://localhost:3000/http_runner_notifications'
     when 'production' then 'http://on-demand.ebu.io/http_runner_notifications'
     else raise "http-runner callback URL not yet configured."
   end
@@ -50,7 +48,6 @@ module EBU
   # http-runner host. Depends on Rails environment and setup.
   HTTP_RUNNER_HOST = case Rails.env
     when 'development' then 'http://localhost:9000'
-    when 'development_remote' then 'http://localhost:9000'
     when 'production' then 'http://localhost:9000'
     else raise "http-runner host not yet configured."
   end
@@ -66,7 +63,6 @@ module EBU
   
   APP_HOST = case Rails.env
     when "production" then "http://on-demand.ebu.io"
-    when "development_remote" then "http://madebyhiro.com:8000"
     else "http://localhost"
   end
 end
--- script/plugit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd vendor/plugit-development-client
-python manage.py runserver
