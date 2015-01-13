1de93bb980e58a4ce6afb69670868d368617d486
Sjoerd Tieleman <sjoerd@madebyhiro.com>
2015-01-13 14:56:35 +0100

    Removed scoping from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,38 @@
 EbuOndemand::Application.routes.draw do
-  scope Rails.application.config.ebu_plugit_local_root do
-    get '/ping', to: "basic#ping"
-    get '/version', to: "basic#version"
+  root 'dashboard#index'
 
-    root 'dashboard#index'
-
-    resources :encoding_jobs do
-      member do
-        get 'status'
-        get 'play'
-        put 'reference_presets'
-        put 'unreference_presets'
-        put 'reference'
-        put 'unreference'
-        put 'reference_source_files'
-        put 'unreference_source_files'
-      end
+  resources :encoding_jobs do
+    member do
+      get 'status'
+      get 'play'
+      put 'reference_presets'
+      put 'unreference_presets'
+      put 'reference'
+      put 'unreference'
+      put 'reference_source_files'
+      put 'unreference_source_files'
     end
-
-    resources :file_assets
-    resources :preset_templates
-    resources :organizations do
-      collection do
-        put 'update_multiple'
-      end
-    end
-    
-    resources :transcoders do
-      member do
-        get 'available'
-      end
-    end
-
-    resources :tags
-    
-    # Dynamic route to send files from public/media. Better done using a Apache
-    # rewrite rule in production! This is only included here as a fallback.
-    get('/media/*other', to: "media#serve", format: false) unless Rails.env.production?
   end
+
+  resources :file_assets
+  resources :preset_templates
+  resources :organizations do
+    collection do
+      put 'update_multiple'
+    end
+  end
+  
+  resources :transcoders do
+    member do
+      get 'available'
+    end
+  end
+
+  resources :tags
+  
+  # Dynamic route to send files from public/media. Better done using a Apache
+  # rewrite rule in production! This is only included here as a fallback.
+  get('/media/*other', to: "media#serve", format: false) unless Rails.env.production?
   
   put  'codem_notifications'       => 'codem_notifications#create'
   post 'http_runner_notifications' => 'http_runner_notifications#create'
