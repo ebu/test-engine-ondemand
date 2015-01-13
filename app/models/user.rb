3c0d8251a4e9b03493cdf598de5ce70012f69643
Sjoerd Tieleman <sjoerd@madebyhiro.com>
2015-01-13 15:45:35 +0100

    Fixed Dashboard (initial).

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   has_many :preset_templates, foreign_key: 'user_id', primary_key: 'ebu_id'
   has_many :file_assets, foreign_key: 'user_id', primary_key: 'ebu_id'
   
-  validates :username, presence: true
-  validates :ebu_id, presence: true
+  validates :uid, presence: true
   
   belongs_to :organization, primary_key: 'ebu_id', foreign_key: 'organization_id'
   
