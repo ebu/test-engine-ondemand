721045b65a3b651e87cad6daf455498feda8e948
Sjoerd Tieleman <sjoerd@madebyhiro.com>
2015-01-14 11:31:53 +0100

    Moved authentication to concern module.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :check_user
-  before_filter :require_login
-
-  private
-  
-  def check_user
-    u = User.find_or_create_by(uid: 'anonymous')
-    u.save
-  end
-  
-  def require_login
-    u = User.find_by(uid: 'anonymous')
-    session['uid'] = u.uid
-  end
+  include Auth
 end
---
+++ app/controllers/concerns/auth.rb
@@ -0,0 +1,24 @@
+module Auth
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :check_user
+    before_filter :require_login
+  end
+  
+  private
+  
+  def check_user
+    u = User.find_or_create_by(uid: 'anonymous')
+    u.save
+  end
+  
+  def require_login
+    u = User.find_by(uid: 'anonymous')
+    session['uid'] = u.uid
+  end
+  
+  def logged_in_user
+    (session && session['uid']) ? User.find_by(uid: session['uid']) : nil
+  end
+end
